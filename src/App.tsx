b346147727d1afbbc23048f0f276dd896ce75aee
Carollayne Cardozo <carollaynecardozo@gmail.com>
2025-03-26 22:25:25 -0300

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import Paper from "@mui/material/Paper";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const columns: GridColDef<[number]>[] = [
+  {
+    field: "id",
+    headerName: "identificador",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "Afastamento",
+    headerName: "Afastamento",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "AnoProcesso",
+    headerName: "Ano do Processo",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "DataAprovacao",
+    headerName: "Data de Aprovação",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "EnquadramentoLegal",
+    headerName: "Enquadramento Legal",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "FornecedorVencedor",
+    headerName: "Fornecedor Vencedor",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "Item",
+    headerName: "Item",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "Objeto",
+    headerName: "Objeto",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "Processo",
+    headerName: "Processo",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "Quantidade",
+    headerName: "Quantidade",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "Unidade",
+    headerName: "Unidade",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "UnidadeMedida",
+    headerName: "Unidade Medida",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "ValorProcesso",
+    headerName: "Valor Processo",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "ValorUnitario",
+    headerName: "Valor Unitário",
+    width: 150,
+    editable: true,
+  },
+];
+
+function App() {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "/api/v1/compras_diretas_estado?limite=1000&csv=false&jsonfull=false",
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = await response.json();
+      const identifiedData = data.map((item: any, index: number) => {
+        return {
+          ...item,
+          id: index + 1,
+        };
+      });
+      setRows(identifiedData);
+    }
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Paper elevation={3}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            },
+          }}
+          pageSizeOptions={[10, 20, 100]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
